c5050e9c8e67511f0cf8c03efcd8345c3b31bf06
alxwlee <alexwlee01@gmail.com>
2018-03-28 04:12:22 -0700

    Initial commit

---
+++ California-State-Template-MVC-v5-5.0/StateTemplateV5Beta/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateTemplateV5Beta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public ActionResult Structure()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult serp()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
